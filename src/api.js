54d9078bda47ec3995906806404e41b65de9658e
Evren Ceyhan <evren@ceyhan.io>
2022-05-01 23:31:16 +0300

    api getMeetups must return array with keys

--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
+import { DUMMY_DATA } from './data';
+
 const API_URL =
     'https://react-meetups-729a4-default-rtdb.europe-west1.firebasedatabase.app/meetups.json';
 
@@ -12,6 +14,15 @@ export const createMeetup = async (meetup) => {
 
 export const getMeetups = async () => {
     const response = await fetch(API_URL);
+    const data = await response.json();
 
-    return response.json();
+    // normalize data as an array
+    return Object.entries(data).map(([id, meetup]) => ({
+        ...meetup,
+        id,
+    }));
+};
+
+export const migrateDummyData = async () => {
+    DUMMY_DATA.forEach(createMeetup);
 };
