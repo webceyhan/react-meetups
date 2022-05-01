2387b43eaa97b558107f3028cb086e07e93fe333
Evren Ceyhan <evren@ceyhan.io>
2022-05-01 21:56:54 +0300

    dummy data updated

--- src/data.js
+++ src/data.js
@@ -1,18 +1,34 @@
 export const DUMMY_DATA = [
     {
         id: 'm1',
-        title: 'This is a first meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+        title: "Women's free pick-up Softball games",
+        image: 'https://secure-content.meetupstatic.com/images/event/aa5c7c8d-270e-4c31-8bfb-d8f94abe98a1/444x250.webp',
+        address:
+            'Central Park north meadow field 12 96st, Central Park · New York, NY',
+        description: 'FREE pick-up Softball games for women.',
     },
     {
         id: 'm2',
-        title: 'This is a second meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
+        title: 'Double Dutch Day',
+        image: 'https://secure-content.meetupstatic.com/images/classic-events/503497589/444x250.webp',
+        address: 'Brooklyn Bridge Park 334 Furman St · Brooklyn, NY',
         description:
-            'This is a second, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+            'Let’s Jump! New school, old school. Beginners, any level jumper is welcome to come!\nBring yourselves and positive energy!',
+    },
+    {
+        id: 'm3',
+        title: 'POSITANO VIRTUAL WALKING TOUR - The most beautiful town on the Amalfi Coast',
+        image: 'https://secure-content.meetupstatic.com/images/classic-events/503003768/444x250.webp',
+        address: 'Online Event',
+        description:
+            'Romantic, alluring and picturesque: this is Positano, one of the most beautiful towns on the Amalfi Coast. Perched precariously on the side of the cliff, its narrow stepped alleys wind between pastel-colored houses and picturesque shops displaying hand-made beachwear and sandals, souvenirs and local culinary products. Everything is enveloped by a magic that conquers the heart and enchants the mind.Are you ready to explore Positano? Sit on your sofa, make yourself a cappuccino and let us take you there!\nOur local guide will take you to discover the beauty of Positano, you will virtually walk through the narrow streets and small alleys full of flowers, you will admire the so famous majolica dome of the main church and the colorful homes suspended over the sea will surely take your breath away.\nDuring this virtual walking tour you’ll understand why visitors from all over the world easily fall in love with every single corner of it and keep coming back after visiting this place for the first time.',
+    },
+    {
+        id: 'm4',
+        title: 'Íber Concierto Series by XOVO',
+        image: 'https://secure-content.meetupstatic.com/images/classic-events/502206842/444x250.webp',
+        address: "Christ & St Stephen's Church 120 W 69th St · New York, NY",
+        description:
+            'A Classical Spanish Guitar Concierto featuring soloist, José Antonio "XOVO" Leon Ruciero. His performance titled, Íber, is a journey through Spain traversing from the romantic Recuerdos de la Alhambra to the lively Asturias.\nThis concert is a set of six performances at Christ & St. Stephen\'s Church through 2022. Each concert will feature some new and some similar pieces. Everyone is welcome to attend each and every one.\nDoors open at 3pm. Performance is at 3:30pm to 5:00pm\nFree Admission. All are welcome!',
     },
 ];
