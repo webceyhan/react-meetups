504c7f334651ca1b3183e4052f885b75ee70ff48
Evren Ceyhan <evren@ceyhan.io>
2022-05-01 21:21:50 +0300

    dummy meetups list added

--- src/pages/AllMeetups.jsx
+++ src/pages/AllMeetups.jsx
@@ -1,3 +1,14 @@
+import { DUMMY_DATA } from '../data';
+
 export const AllMeetupsPage = () => {
-    return <div>AllMeetups Page</div>;
+    return (
+        <div>
+            <h1>All Meetups</h1>
+            <ul>
+                {DUMMY_DATA.map((meetup) => (
+                    <li key={meetup.id}>{meetup.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
 };
