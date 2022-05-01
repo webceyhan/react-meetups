fc86001ab7eadd9d56e9dc7c12de8b6e15ae9e98
Evren Ceyhan <evren@ceyhan.io>
2022-05-02 00:20:31 +0300

    conditional rendering on favorites page

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -4,10 +4,18 @@ import { useFavoritesContext } from '../store/FavoritesContext';
 export const FavoritesPage = () => {
     const favoritesCtx = useFavoritesContext();
 
+    let content;
+
+    if (favoritesCtx.favorites.length === 0) {
+        content = <p>You got no favorites yet, Start adding some?</p>;
+    } else {
+        content = <MeetupList meetups={favoritesCtx.favorites} />;
+    }
+
     return (
         <section>
             <h5>My Favorites</h5>
-            <MeetupList meetups={favoritesCtx.favorites} />
+            {content}
         </section>
     );
 };
