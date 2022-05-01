b0247e68ac8438e18d495212e319714057b0d514
Evren Ceyhan <evren@ceyhan.io>
2022-05-01 23:31:38 +0300

    dummy data replaced by api data

--- src/pages/AllMeetups.jsx
+++ src/pages/AllMeetups.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from 'react';
+import { getMeetups } from '../api';
 import { MeetupList } from '../components/MeetupList';
-import { DUMMY_DATA } from '../data';
 
 export const AllMeetupsPage = () => {
+    const [loading, setLoading] = useState(true);
+    const [meetups, setMeetups] = useState([]);
+
+    // note: without useEffect component will be re-rendered 
+    // on every change of meetups which will cause the infinite loop
+    useEffect(() => {
+        getMeetups()
+            .then(setMeetups)
+            .then(() => setLoading(false));
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div>
             <h1>All Meetups</h1>
-            <MeetupList meetups={DUMMY_DATA} />
+            <MeetupList meetups={meetups} />
         </div>
     );
 };
